f99589b57fdadb2cb3960e7c9d7d1b27c4dcc6ad
Arthur-Jr <arthurjr1906@gmail.com>
2022-03-27 12:34:26 -0300

    feat: Adiciona a rota do cpf na api.

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,9 +1,21 @@
+const bodyParser = require('body-parser');
 const express = require('express');
+const cors = require('cors');
+
+const errorMiddleware = require('../middlewares/errorMiddleware');
+const cpfRouter = require('../msc-layers/routers/cpf.router');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3001;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use('/cpf', cpfRouter); /* Registro, edição e remoção de CPF */
+// app.use('/cnpj');
+// app.get('/cpf-cnpj-lists');
+
+app.use(errorMiddleware);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
