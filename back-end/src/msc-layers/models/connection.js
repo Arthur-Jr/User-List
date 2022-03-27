f502a812c5fdb9f9a17b818dc178c9ecb4bc1a99
Arthur-Jr <arthurjr1906@gmail.com>
2022-03-27 12:38:02 -0300

    feat: Configura a conexão com o MongoDB.

---
+++ back-end/src/msc-layers/models/connection.js
@@ -0,0 +1,15 @@
+const mongodb = require('mongodb').MongoClient;
+require('dotenv').config();
+
+const MONGO_DB_URL = process.env.DB_URL || 'mongodb://localhost:27017/cpf/cnpj-validation';
+const DB_NAME = 'cpf-cnpj-list';
+
+module.exports = () => mongodb.connect(MONGO_DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then((connection) => connection.db(DB_NAME))
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
