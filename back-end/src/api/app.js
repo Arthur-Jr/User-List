ff8d74a4e8fbb329f2b71109497cc130b2d48709
Arthur-Jr <arthurjr1906@gmail.com>
2022-03-27 22:52:36 -0300

    feat: Adiciona a rota de status no app.

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -6,6 +6,11 @@ const errorMiddleware = require('../middlewares/errorMiddleware');
 const cpfRouter = require('../msc-layers/routers/cpf.router');
 const cnpjRouter = require('../msc-layers/routers/cnpj.router');
 const { getAllCpfCnpjList } = require('../msc-layers/controllers/getList.controller');
+const incrementReqCountController = require('../middlewares/IncrementReqCountMiddleware');
+const {
+  setServerStatusController,
+  getServerStatusController,
+} = require('../msc-layers/controllers/serverStatus.controller');
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -13,12 +18,17 @@ const port = process.env.PORT || 3001;
 app.use(bodyParser.json());
 app.use(cors());
 
+app.get('/status', getServerStatusController); /* Consultar o status do server */
+
+app.use(incrementReqCountController); /* Incrementa o número de req */
+
 app.use('/cpf', cpfRouter); /* Registro, edição e remoção de CPF */
 app.use('/cnpj', cnpjRouter); /* Registro, edição e remoção de CNPJ */
 app.get('/cpf-cnpj-lists', getAllCpfCnpjList); /* Consulta de todos CPF/CNPJ */
 
 app.use(errorMiddleware);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+setServerStatusController(); /* Salva a data em que o server foi iniciado */
+app.listen(port, async () => console.log(`Example app listening on port ${port}!`));
 
 module.exports = app;
