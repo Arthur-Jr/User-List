bb7c5dfeef546ac46eec94ed3330a7ad02b6831a
Arthur-Jr <arthurjr1906@gmail.com>
2022-03-30 01:40:12 -0300

    feat: Adiciona sort no retorno da requisiçao

--- front-end/src/api/getAll-cpf-cnpj.js
+++ front-end/src/api/getAll-cpf-cnpj.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 
+import sortList from '../utils/sortList';
+
 const URL = 'http://localhost:3001/';
 
 const getAllCpfCnpj = async () => {
   try {
-    const { data } = await axios.get(`${URL}cpf-cnpj-lists`);
-    return data;
+    const { data: { cpf, cnpj } } = await axios.get(`${URL}cpf-cnpj-lists`);
+    return { cpf: sortList(cpf, 'cpf'), cnpj: sortList(cnpj, 'cnpj') };
   } catch (error) {
     return { cpf: [], cnpj: [] };
   }
