06285edd215e247cb368ead51c5301a53be89272
Arthur-Jr <arthurjr1906@gmail.com>
2022-03-30 02:32:31 -0300

    feat: Adiciona a regra de negocio do filtro da pagina de consulta

--- front-end/src/pages/consult-cpf-cnpj.jsx
+++ front-end/src/pages/consult-cpf-cnpj.jsx
@@ -1,28 +1,113 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import getAllCpfCnpj from '../api/getAll-cpf-cnpj';
+import editCpfCnpj from '../api/edit-cpf-cnpj';
+import removeCpfCnpj from '../api/remove-cpf-cnpj';
 import RadioInputsSection from '../components/RadioInputsSection.jsx';
+import TextInputSection from '../components/TextInputSection.jsx';
+import sortList from '../utils/sortList';
+import ListCpfCnpj from '../components/ListCpfCnpj.jsx';
+import setMessageWithTime from '../utils/setMessageWithTimer';
 
 function ConsultCpfCnpj() {
   const [allCpfCnpj, setAllCpfCnpj] = useState({ cpf: [], cnpj: [] });
   const [arrayToDisplay, setArrayToDisplay] = useState([]);
   const [radioValue, setRadioValue] = useState('cpf/cnpj');
+  const [textInputValue, setTextInputValue] = useState('');
+  const [blockStatus, setBlockStatus] = useState(false);
+  const [responseMessage, setResponseMessage] = useState('');
 
+  // Assim que o componente carrega Faz uma requisição para pegar a lista de CPF/CNPJ.
   useEffect(() => {
     getAllCpfCnpj().then((response) => setAllCpfCnpj(response));
   }, []);
 
+  // Lida com os filtros:
   useEffect(() => {
+    let toDisplay;
+
+    // Filtro do radio input:
     if (radioValue === 'cpf/cnpj') {
-      setArrayToDisplay([...allCpfCnpj.cpf, ...allCpfCnpj.cnpj]);
+      toDisplay = [...allCpfCnpj.cpf, ...allCpfCnpj.cnpj];
     } else {
-      setArrayToDisplay(allCpfCnpj[radioValue]);
+      toDisplay = allCpfCnpj[radioValue];
     }
-  }, [radioValue, allCpfCnpj]);
+
+    // Filtro do text input:
+    if (textInputValue.length > 0) {
+      toDisplay = toDisplay.filter((cpfCpnj) => {
+        const filterResult = cpfCpnj.cpf ? cpfCpnj.cpf.includes(textInputValue)
+          : cpfCpnj.cnpj.includes(textInputValue);
+        return filterResult;
+      });
+    }
+
+    // Filtro do checkbox input:
+    if (blockStatus) {
+      toDisplay = toDisplay.filter(({ blockListed }) => blockListed);
+    }
+
+    setArrayToDisplay(toDisplay);
+  }, [radioValue, allCpfCnpj, textInputValue, blockStatus]);
+
+  // Certifica que o input de texto só vai aceitar números:
+  const handleInputTextChange = ({ target: { value } }) => {
+    const onlyNumberRegex = /(^[0-9]*$)|([.-]*$)/;
+    if (onlyNumberRegex.test(value)) setTextInputValue(value);
+  };
+
+  // Edita o CPF/CNPJ:
+  const handleEdit = async ({ target: { value, checked } }) => {
+    const cpfOrCnpj = value.length === 11 ? 'cpf' : 'cnpj';
+    const editedData = await editCpfCnpj(checked, value, cpfOrCnpj);
+
+    if (editedData[cpfOrCnpj]) {
+      const takeOutEdited = allCpfCnpj[cpfOrCnpj]
+        .filter((data) => data[cpfOrCnpj] !== editedData[cpfOrCnpj]);
+
+      const arrayWithEdited = sortList([...takeOutEdited, editedData], cpfOrCnpj);
+
+      setAllCpfCnpj({ ...allCpfCnpj, [cpfOrCnpj]: arrayWithEdited });
+    } else {
+      setMessageWithTime(editedData, setResponseMessage, 5000);
+    }
+  };
+
+  // Remove CPF/CNPJ:
+  const handleRemove = async (cpfCnpjToRemove) => {
+    const cpfOrCnpj = cpfCnpjToRemove.length === 11 ? 'cpf' : 'cnpj';
+    const message = await removeCpfCnpj(cpfCnpjToRemove, cpfOrCnpj);
+
+    const takeOutRemoved = allCpfCnpj[cpfOrCnpj]
+      .filter((data) => data[cpfOrCnpj] !== cpfCnpjToRemove);
+
+    setAllCpfCnpj({ ...allCpfCnpj, [cpfOrCnpj]: takeOutRemoved });
+
+    setMessageWithTime(message, setResponseMessage, 5000);
+  };
 
   return (
-  <main>
+  <main className="consult-main">
     <RadioInputsSection setRadioValue={ setRadioValue } registerPage={ false } />
+    <TextInputSection
+      textInputValue={ textInputValue }
+      radioValue={ radioValue }
+      handleInputTextChange={ handleInputTextChange }
+      blockStatus={ blockStatus }
+      setBlockStatus={ setBlockStatus }
+    />
+
+    <Link to="/register-cpf-cnpj">Adicionar novo CPF/CNPJ</Link>
+
+    <ListCpfCnpj
+      arrayToDisplay={ arrayToDisplay }
+      handleEdit={ handleEdit }
+      handleRemove={ handleRemove }
+    />
+
+    { responseMessage.length !== 0
+    && <span data-testid="response-message">{ responseMessage }</span> }
   </main>
   );
 }
