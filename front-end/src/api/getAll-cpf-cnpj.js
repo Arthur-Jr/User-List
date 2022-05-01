a6cdd208459d33bdaf7075c056ec123b2265fae5
Arthur-Jr <arthurjr1906@gmail.com>
2022-04-30 22:19:19 -0300

    fix: Altera a funcao de sort

--- front-end/src/api/getAll-cpf-cnpj.js
+++ front-end/src/api/getAll-cpf-cnpj.js
@@ -7,7 +7,7 @@ const URL = 'http://localhost:3001/';
 const getAllCpfCnpj = async () => {
   try {
     const { data: { cpf, cnpj } } = await axios.get(`${URL}cpf-cnpj-lists`);
-    return { cpf: sortList(cpf, 'cpf'), cnpj: sortList(cnpj, 'cnpj') };
+    return { cpf, cnpj };
   } catch (error) {
     return { cpf: [], cnpj: [] };
   }
