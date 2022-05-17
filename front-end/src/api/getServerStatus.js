35ccbd00adc5e3206c4215c8845c3519940415b5
Arthur-Jr <arthurjr1906@gmail.com>
2022-05-17 20:25:01 -0300

    fix: fix server end-point

--- front-end/src/api/getServerStatus.js
+++ front-end/src/api/getServerStatus.js
@@ -4,7 +4,7 @@ const URL = process.env.REACT_APP_BACK_ENDPOINT || 'http://localhost:3001';
 
 const getServerStatus = async () => {
   try {
-    const { data: { upTime, reqCount } } = await axios.get(`${URL}status`);
+    const { data: { upTime, reqCount } } = await axios.get(`${URL}/status`);
     return `Tempo Online(H/M/S): ${upTime}, Quantidade de consultas: ${reqCount}`;
   } catch (error) {
     return error.response.data.message;
